22a748df1ce4e5dd7fe9d0d5411f8933fa80086a
Mardrey <mardreylo@gmail.com>
2013-04-16 16:52:51 +0200

    added example that reads some files, and has necessary .jars

--- Pathogenum/src/Pathogenum.java
+++ Pathogenum/src/Pathogenum.java
@@ -1,11 +1,61 @@
+import java.io.File;
+import java.util.ArrayList;
 
-public class Pathogenum {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 
+
+public class Pathogenum extends BasicGame{
+
+	ArrayList<Image> images;
+	
+	public Pathogenum(String title) {
+		super(title);
+		images = new ArrayList<Image>();
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		for(Image img: images){
+			img.draw();
+		}
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		readImages("resources/");
+		
+	}
+
+	private void readImages(String dir) throws SlickException {
+		File res_dir = new File(dir);
+		String[] files = res_dir.list();
+		for(String s: files){
+			Image im = new Image(dir+s);
+			images.add(im);
+		}
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	/**
 	 * @param args
+	 * @throws SlickException 
 	 */
-	public static void main(String[] args) {
-		System.out.println("BACTERIA, FUCK YEAH!");
+	public static void main(String[] args) throws SlickException {
+		Pathogenum petriDish = new Pathogenum("Pathogenum");
+		AppGameContainer agc = new AppGameContainer(petriDish);
+		agc.setTargetFrameRate(60);
+		agc.start();
 	}
 
 }
