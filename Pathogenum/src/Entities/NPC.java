6ddf74a5246f26c8ec62c0062ffa73fefbae9314
Marie <ada09mve@student.lth.se>
2013-04-22 12:59:50 +0200

    NPC växer också vid krock

--- Pathogenum/src/Entities/NPC.java
+++ Pathogenum/src/Entities/NPC.java
@@ -8,6 +8,7 @@ import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.BodyType;
 import org.jbox2d.dynamics.FixtureDef;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Shape;
 
 import utils.Dimensions;
@@ -57,13 +58,19 @@ public class NPC extends Entity {
 				acc[i] = rand.nextInt(2);
 			}
 		} 
-
+		changeShapeSize();
 		Vec2 f = new Vec2( (force * ms) * (acc[3]-acc[2]), (force* ms) * (acc[1] - acc[0]));
 		Vec2 curr_vec = body.getLinearVelocity();
 		Vec2 new_vec = new Vec2(curr_vec.x + (f.x) , curr_vec.y + (f.y));		
 		body.setLinearVelocity(new_vec);
 		forceCount--;
 
+	}
+	
+	public void changeShapeSize(){
+		Circle c = (Circle) sh;
+		float r = body.getFixtureList().getShape().getRadius();
+		c.setRadius(1000*Dimensions.pixelToMeter(r));
 	}
 
 
