94b335abe20a15229e7f82f1e23507802985746f
BigFarmor <xarac1337@gmail.com>
2013-04-23 11:44:10 +0200

    Should be error free now

--- Pathogenum/src/client/InputThread.java
+++ Pathogenum/src/client/InputThread.java
@@ -9,65 +9,65 @@ import java.util.LinkedList;
 import utils.Conversions;
 
 public class InputThread extends Thread {
-	Socket sock;
-	InputStream is;
-	LinkedList<String> chatBuffer = new LinkedList<String>();
-	public InputThread(Socket sock){
-		this.sock = sock;
-		try {
-			is = sock.getInputStream();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	public void run(){
-		while(true){
-			byte[] command = new byte[4];
-			try {		
-				int intCommand = Conversions.ByteArrayToInt(command);
-				switch(intCommand){
-				case ClientConnectionHandler.SENDMESSAGE:
-				
-					int check = is.read(command);		
-					if(checkInput(check))
-						return;
-					intCommand = Conversions.ByteArrayToInt(command);
-					byte[] input = new byte[intCommand];
-					
-					check = is.read(input);	
-					if(checkInput(check))
-						return;
-					String text = new String(input);
-					chatBuffer.offer(text);
-					break;
-				}
-				
-			} catch (IOException e) {
-		
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	private boolean checkInput(int check) {
-		if(check == -1){
-			try {
-				sock.close();
-				return true;
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return false;
-	}
-	public ArrayList<String> getChatMessages() {
+    Socket sock;
+    InputStream is;
+    LinkedList<String> chatBuffer = new LinkedList<String>();
+    public InputThread(Socket sock){
+        this.sock = sock;
+        try {
+            is = sock.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void run(){
+        while(true){
+            byte[] command = new byte[4];
+            try {
+                is.read(command);
+                int intCommand = Conversions.ByteArrayToInt(command);
+                switch(intCommand){
+                case ClientConnectionHandler.SENDMESSAGE:
+                   
+                    int check = is.read(command);       
+                    if(checkInput(check))
+                        return;
+                    intCommand = Conversions.ByteArrayToInt(command);
+                    byte[] input = new byte[intCommand];
+                    check = is.read(input);   
+                    if(checkInput(check))
+                        return;
+                    String text = new String(input);
+                    chatBuffer.offer(text);
+                    break;
+                }
+               
+            } catch (IOException e) {
+       
+                e.printStackTrace();
+            }
+        }
+    }
+   
+    private boolean checkInput(int check) {
+        if(check == -1){
+            try {
+                sock.close();
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    public ArrayList<String> getChatMessages() {
 
-		ArrayList<String> list = new ArrayList<String>();
-		int size = chatBuffer.size();
-		for(int i = 0; i < size; i++){
-			list.add(chatBuffer.pop());
-		}
-		return list;
-	}
-	
+        ArrayList<String> list = new ArrayList<String>();
+        int size = chatBuffer.size();
+        for(int i = 0; i < size; i++){
+            list.add(chatBuffer.pop());
+        }
+        return list;
+    }
+   
 }
